200ac53d7e8c62ad1e1925e708ab74317f23e247
Ville Strengell <ville.strengell@aalto.fi>
2025-01-17 13:25:47 +0200

    deployed but not found?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header";
+import Header from "./__components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Hero from './components/Hero'
-import Skills from './components/Skills'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
-import Workexperience from './components/Workexperience'
+import Hero from './__components/Hero'
+import Skills from './__components/Skills'
+import Projects from './__components/Projects'
+import Contact from './__components/Contact'
+import Footer from './__components/Footer'
+import Workexperience from './__components/Workexperience'
 
 export default function Home() {
   return (
